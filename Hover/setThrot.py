45a7cc5549d2e9ae0ca5d93bcaad47bce28ace1b
PCote <pcote@mtech.edu>
2018-02-11 11:50:54 -0700

    Add user input display, add pure sine output

--- Hover/setThrot.py
+++ Hover/setThrot.py
@@ -19,7 +19,7 @@ minThrottle = 1400
 height = 0
 throttle = 1000
 trange = maxThrottle-minThrottle
-dt = .001
+dt = .001  # Delay for throttle ramps
 
 # Function for set throttle thread
 def setThrottle():
@@ -29,22 +29,24 @@ def setThrottle():
         dist = sensor.getDistanceCM()
         if(dist > 4 and dist < 400):
             height = dist
-        if throttle:
+        if throttle>0:
             throttle = max(min(throttle,maxThrottle),1000)
-        else:
-            pass
+        elif throttle<0:
             if armDrone:
                 canary.disarm()
+                break
+        else:
+            pass
         try:
             if armDrone:
                 canary.setThrottle(throttle)
         except:
-            pass
-	data = str(time())+','+str(height)+','+str(throttle)+',\n'
+            print "Throttle set error"
+        data = str(time())+','+str(height)+','+str(throttle)+',\n'
         try:
-	    f.write(data)
+            f.write(data)
         except:
-            pass
+            print "Data write error"
         sleep(.06)
 
 # Init
@@ -65,55 +67,76 @@ sleep(.11)
 #   If <ctrl - c>, disarm and quit
 try:
     while True:
-		print "Height: ",height," cm"
-		print "throttle: ",throttle
-                tin = input("Set Throttle: ")
+        # User Input Menu
+        print "----- Current Status -----"
+        print "   Height   : ",height," cm"
+        print "   Throttle : ",throttle
+        print "----- Input Options ------"
+        print "Sinusoid: 1-9 -- Sine wave of frequency 1-9Hz"
+        print "Ramp: 10-Up, 20-Down, 30-Up/Down, 40-Up/Down (1/2 speed) , 50-Chirp (0-8Hz)"
+        print "Set Throttle: ",minThrottle," to ", maxThrottle
+        print "Disarm: 0"
+        print "Exit: -1 or <ctrl>+c"
 
-                if tin == 0:
-                    if armDrone:
-                        canary.disarm()
-                    exit()
-                elif tin == 1: # ramp up
-                    for i in range(minThrottle,maxThrottle):
-                        throttle = i
-                        print throttle
-                        sleep(dt)
-                elif tin == 2: # ramp down
-                    for i in range(maxThrottle,minThrottle,-1):
-                        throttle = i
-                        print throttle
-                        sleep(dt)
-                elif tin == 3: # up then down
-                    for i in range(minThrottle,maxThrottle):
-                        throttle = i
-                        print throttle
-                        sleep(dt)
-                    for i in range(maxThrottle,minThrottle,-1):
-                        throttle = i
-                        print throttle
-                        sleep(dt)
-                elif tin == 4: # up then down half speed
-                    for i in range(minThrottle,maxThrottle):
-                        throttle = i
-                        print throttle
-                        sleep(dt*2)
-                    for i in range(maxThrottle,minThrottle,-1):
-                        throttle = i
-                        print throttle
-                        sleep(dt*2)
-                elif tin == 5: # chirp to 10Hz
-                    for i in range(0,int(10/dt)):
-                        throttle = math.sin(i*i*3.14159*dt)*trange + minThrottle + (trange/2)
-                        print throttle
-                        sleep(dt)
-                else:
-                    throttle = tin
-    f.close()
-    if armDrone:
-        canary.disarm()
+        tin = input("\nInput: ")
+        
+        # 
+        if tin == 0:
+            if armDrone:
+                canary.disarm()
+        elif tin == 10: # ramp up
+            for i in range(minThrottle,maxThrottle):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+        elif tin == 20: # ramp down
+            for i in range(maxThrottle,minThrottle,-1):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+        elif tin == 30: # up then down
+            for i in range(minThrottle,maxThrottle):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+            for i in range(maxThrottle,minThrottle,-1):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+        elif tin == 40: # up then down half speed
+            for i in range(minThrottle,maxThrottle):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt*2)
+            for i in range(maxThrottle,minThrottle,-1):
+                throttle = i
+                print "Throttle Ramping: ",throttle
+                sleep(dt*2)
+        elif tin == 50: # chirp to 8Hz
+            for i in range(0,int(8/dt)):
+                throttle = math.sin(i*i*3.14159*dt)*trange + minThrottle + (trange/2)
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+        elif 0 < tin and tin < 10: # Sinusoid
+            for i in range(0,int(5/dt)):
+                throttle = math.sin(tin*2*3.14159*dt)*trange + minThrottle + (trange/2)
+                print "Throttle Ramping: ",throttle
+                sleep(dt)
+        elif tin < 0:
+            f.close()
+            if armDrone:
+                canary.disarm()
+            GPIO.cleanup()
+            throttle = tin
+            break
+        else:
+            throttle = tin
+    canary.disarm()
     GPIO.cleanup()
+    exit()
 except KeyboardInterrupt:
-    throttle = 0
+    f.close()
+    throttle = -1
     canary.disarm()
     GPIO.cleanup()
     exit()
