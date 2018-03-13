3fc9f4e29ffa9eb5eb332a80beeced37d3b80cdd
PI3 Canary1 <pcote@mtech.edu>
2018-03-13 03:46:12 +0000

    Added single channel read

--- i2cSense/testI2cSlave.py
+++ i2cSense/testI2cSlave.py
@@ -10,14 +10,23 @@ class SonicComm:
 
     def write(self,cmd):
         self.bus.write_i2c_block_data(self.address, 0 , map(ord,cmd))
-    
+
+    def readOne(self):
+        self.readArray =  self.bus.read_i2c_block_data(self.address, 3)
+        print self.readArray[0]
+
     def read(self):
         self.readArray =  self.bus.read_i2c_block_data(self.address, 3)
         print self.readArray[0:6]
 
 
 x = SonicComm(0x52)
-x.write("d")
-time.sleep(.01)
-x.read()
-exit()
+while 1:
+	x.write("A")
+	time.sleep(.01)
+	x.read()
+	time.sleep(.1)
+	x.write("1")
+	time.sleep(.01)
+	x.readOne()
+	time.sleep(.1)
