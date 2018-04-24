4564235decbf60940697f2462993054bd215baae
PI3 Canary1 <pcote@mtech.edu>
2018-04-24 16:08:17 +0000

    Fixed co2 call to add logging dir

--- Hover/kevinDemoNazaCO2.py
+++ Hover/kevinDemoNazaCO2.py
@@ -183,7 +183,10 @@ GPIO.output(STATUS_LED, GPIO.LOW)
 sleep(.1)
 
 # Init the CO2 detection w/ printing and logging
-co2 = CO2Comm(STATUS_LED,1,1)
+co2Dir = FILE_DIR
+if armDrone == 0:
+	co2Dir = co2Dir + 'x'
+co2 = CO2Comm(STATUS_LED,1,1,co2Dir)
 sleep(.1)
 
 # --------------- Takeoff Sequence---------------------------------------------
